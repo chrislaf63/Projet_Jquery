06369fcfba7f7c1c53c17a8618e6c0245864ec2d
chrislaf63 <chrislaf63@gmail.com>
2023-10-17 17:44:56 +0200

    Animation ouverture + correction erreur CSS

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ $(function(){
 
     var $mainMenuItems = $("#main-menu ul").children("li"),
         totalMainMenuItems = $mainMenuItems.length,
-        openedIndex = -1,
+        openedIndex = 2,
         init = function(){
+            bindEvents();
+            if(validIndex(openedIndex))
+                animateItem($mainMenuItems.eq(openedIndex), true, 700);      
+          },
 
+          bindEvents = function(){
             $mainMenuItems.children(".images").click(function(){
                 var newIndex = $(this).parent().index();
                 $item = $mainMenuItems.eq(newIndex);
@@ -22,7 +27,7 @@ $(function(){
                 
 
             });
-          },
+          }
 
           validIndex = function(indexToCheck) {
             return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
